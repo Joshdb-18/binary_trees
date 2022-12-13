6afa1257c67e8b481fd55bea2e46f7b6231b16f7
Joshdb-18 <joshuadami17@gmail.com>
2022-12-13 10:17:23 +0000

    binary tree insert right

--- binary_trees.h
+++ binary_trees.h
@@ -36,5 +36,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 /* binary tree insert left func */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 
+/* binary tree insert right func */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
 #endif /* _BINARY_TREES_H_ */
 
