aba87596617e3c792f23ab911c30910fc9910902
Joshdb-18 <joshuadami17@gmail.com>
2022-12-13 09:25:09 +0000

    binary tree insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	binary_tree_t *node;
 
 	if (parent == NULL)
-		return(NULL);
+		return (NULL);
 
 	node = binary_tree_node(parent, value);
 	if (node == NULL)
