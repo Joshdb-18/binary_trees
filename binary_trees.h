210f371f0df55f7af8abb536bc8649d315d845b2
Joshdb-18 <joshuadami17@gmail.com>
2022-12-14 12:25:59 +0000

    binary tree depth

--- binary_trees.h
+++ binary_trees.h
@@ -60,5 +60,8 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 /* binary tree height func */
 size_t binary_tree_height(const binary_tree_t *tree);
 
+/* binary tree depth func */
+size_t binary_tree_depth(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
 
