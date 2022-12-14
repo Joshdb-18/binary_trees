726498a797dafe08da395722b0b0fc94e62808bc
Joshdb-18 <joshuadami17@gmail.com>
2022-12-14 11:22:11 +0000

    binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,6 +4,7 @@
  * binary_tree_node - creates a binary tree node
  * @parent: pointer to the parent node of the node to create
  * @value: value to place in new node
+ *
  * Return: pointer to the new node, or NULL on failure
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
