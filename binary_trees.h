45795b4b81085cd7c52c458185d7a86961d3d6e7
Joshdb-18 <joshuadami17@gmail.com>
2022-12-14 12:08:34 +0000

    binary tree post-order traversal

--- binary_trees.h
+++ binary_trees.h
@@ -54,5 +54,8 @@ void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
 /* binary tree inorder func */
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 
+/* binary tree postorder func */
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
+
 #endif /* _BINARY_TREES_H_ */
 
