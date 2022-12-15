ceaf6e3b1609979fab6fc84e6b5548c7d00cf01e
Joshdb-18 <joshuadami17@gmail.com>
2022-12-15 15:42:10 +0000

    binary tree is BST

--- binary_trees.h
+++ binary_trees.h
@@ -102,9 +102,13 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 /* binary tree rotate right function */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
 
+/* binary tree is bst function */
+int binary_tree_is_bst(const binary_tree_t *tree);
+
 /* helper functions */
 void binary_tree_levelorder_helper(const binary_tree_t *tree, void (*func)(int), size_t level);
 int binary_tree_is_complete_helper(const binary_tree_t *tree, size_t index, size_t size);
+int binary_tree_is_bst_helper(const binary_tree_t *tree, int min, int max);
 
 #endif /* _BINARY_TREES_H_ */
 
